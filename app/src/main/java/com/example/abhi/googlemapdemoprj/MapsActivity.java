a713ffa71eb7c83f1049d4d1fa4eb9fb38447ae3
Arpitha1 <arpitha.gr01@gmail.com>
2017-06-26 09:22:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhi/googlemapdemoprj/MapsActivity.java
@@ -0,0 +1,44 @@
+package com.example.abhi.googlemapdemoprj;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final LatLng DAVAO = new LatLng(12.9699, 77.6499);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Marker davao = googleMap.addMarker(new MarkerOptions().position(DAVAO).title("Indiranagar").snippet("Bangalore"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DAVAO, 15));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+    }
+}
